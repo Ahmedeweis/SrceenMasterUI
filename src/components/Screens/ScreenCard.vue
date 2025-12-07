18db812bd62b95d2e40741139d492c421ea9ae34
Ahmedeweis <93427804+Ahmedeweis@users.noreply.github.com>
2025-12-07 18:06:54 +0200

    ..

--- src/components/Screens/ScreenCard.vue
+++ src/components/Screens/ScreenCard.vue
@@ -29,7 +29,7 @@
   </div>
 </template>
 <script setup>
-import ScreenService from "../../services/screenService";
+import ScreenService from "../../services/screen.js";
 import Swal from "sweetalert2";
 const props = defineProps({
   screen: { type: Object, required: true },
